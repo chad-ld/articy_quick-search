762703a0a600bb04506b94512dd90c0b8d5d888d
Steven Kiesewetter <steven@elementxcreative.com>
2023-06-06 09:44:52 -0500

    Initial commit of RND project (currently improperly named TabToFocus)

---
+++ LastDragonMedia.TabToFocus/LastDragonMedia.TabToFocus/MouseHook.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows;
+
+namespace GlobalLowLevelHooks
+{
+    /// <summary>
+    /// Class for intercepting low level Windows mouse hooks.
+    /// </summary>
+    class MouseHook
+    {
+        /// <summary>
+        /// Internal callback processing function
+        /// </summary>
+        private delegate IntPtr MouseHookHandler(int nCode, IntPtr wParam, IntPtr lParam);
+        private MouseHookHandler hookHandler;
+
+        /// <summary>
+        /// Function to be called when defined even occurs
+        /// </summary>
+        /// <param name="mouseStruct">MSLLHOOKSTRUCT mouse structure</param>
+        public delegate void MouseHookCallback(MSLLHOOKSTRUCT mouseStruct);
+
+        #region Events
+        public event MouseHookCallback LeftButtonDown;
+        public event MouseHookCallback LeftButtonUp;
+        public event MouseHookCallback RightButtonDown;
+        public event MouseHookCallback RightButtonUp;
+        public event MouseHookCallback MouseMove;
+        public event MouseHookCallback MouseWheel;
+        public event MouseHookCallback DoubleClick;
+        public event MouseHookCallback MiddleButtonDown;
+        public event MouseHookCallback MiddleButtonUp;
+        #endregion
+
+        /// <summary>
+        /// Low level mouse hook's ID
+        /// </summary>
+        private IntPtr hookID = IntPtr.Zero;
+
+        /// <summary>
+        /// Install low level mouse hook
+        /// </summary>
+        /// <param name="mouseHookCallbackFunc">Callback function</param>
+        public void Install()
+        {
+            hookHandler = HookFunc;
+            hookID = SetHook(hookHandler);
+        }
+
+        /// <summary>
+        /// Remove low level mouse hook
+        /// </summary>
+        public void Uninstall()
+        {
+            if (hookID == IntPtr.Zero)
+                return;
+
+            UnhookWindowsHookEx(hookID);
+            hookID = IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Destructor. Unhook current hook
+        /// </summary>
+        ~MouseHook()
+        {
+            Uninstall();
+        }
+
+        /// <summary>
+        /// Sets hook and assigns its ID for tracking
+        /// </summary>
+        /// <param name="proc">Internal callback function</param>
+        /// <returns>Hook ID</returns>
+        private IntPtr SetHook(MouseHookHandler proc)
+        {
+            using (ProcessModule module = Process.GetCurrentProcess().MainModule)
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(module.ModuleName), 0);
+        }
+
+        /// <summary>
+        /// Callback function
+        /// </summary>
+        private IntPtr HookFunc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            // parse system messages
+            if (nCode >= 0)
+            {
+                if (MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+                    if (LeftButtonDown != null)
+                        LeftButtonDown((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_LBUTTONUP == (MouseMessages)wParam)
+                    if (LeftButtonUp != null)
+                        LeftButtonUp((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_RBUTTONDOWN == (MouseMessages)wParam)
+                    if (RightButtonDown != null)
+                        RightButtonDown((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_RBUTTONUP == (MouseMessages)wParam)
+                    if (RightButtonUp != null)
+                        RightButtonUp((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
+                    if (MouseMove != null)
+                        MouseMove((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_MOUSEWHEEL == (MouseMessages)wParam)
+                    if (MouseWheel != null)
+                        MouseWheel((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_LBUTTONDBLCLK == (MouseMessages)wParam)
+                    if (DoubleClick != null)
+                        DoubleClick((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_MBUTTONDOWN == (MouseMessages)wParam)
+                    if (MiddleButtonDown != null)
+                        MiddleButtonDown((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+                if (MouseMessages.WM_MBUTTONUP == (MouseMessages)wParam)
+                    if (MiddleButtonUp != null)
+                        MiddleButtonUp((MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT)));
+            }
+            return CallNextHookEx(hookID, nCode, wParam, lParam);
+        }
+
+        #region WinAPI
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205,
+            WM_LBUTTONDBLCLK = 0x0203,
+            WM_MBUTTONDOWN = 0x0207,
+            WM_MBUTTONUP = 0x0208
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+
+            public static implicit operator POINT(Point point)
+            {
+                return new POINT() { X=(int)point.X, Y=(int)point.Y};
+            }
+
+            public static implicit operator POINT(System.Drawing.Point point)
+            {
+                return new POINT() { X = (int)point.X, Y = (int)point.Y };
+            }
+
+            public static POINT operator /(POINT point, float b)
+            {
+                return new POINT() { X = (int)(point.X/b), Y = (int)(point.Y/b) };
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the cursor's position, in screen coordinates.
+        /// </summary>
+        /// <see>See MSDN documentation for further information.</see>
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetPhysicalCursorPos(out POINT lpPoint);
+
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+        public enum DeviceCap
+        {
+            VERTRES = 10,
+            DESKTOPVERTRES = 117,
+
+            // http://pinvoke.net/default.aspx/gdi32/GetDeviceCaps.html
+        }
+
+
+        public static float GetScalingFactor()
+        {
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr desktop = g.GetHdc();
+            int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+            int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+            float ScreenScalingFactor = (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+
+            return ScreenScalingFactor; // 1.25 = 125%
+        }
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetPhysicalCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint / GetScalingFactor(); ;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            MouseHookHandler lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        #endregion
+    }
+}
---
+++ LastDragonMedia.TabToFocus/LastDragonMedia.TabToFocus/Plugin.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Media;
+using GlobalLowLevelHooks;
+using System;
+using Articy.Utils.Manifests;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Articy.Api;
+using Articy.Api.Plugins;
+
+//using Texts = LIds.LastDragonMedia.TabToFocus;
+
+namespace LastDragonMedia.TabToFocus
+{
+    /// <summary>
+    /// public implementation part of plugin code, contains all overrides of the plugin class.
+    /// </summary>
+    public partial class Plugin : MacroPlugin
+    {
+        public override string DisplayName => "Hotkey to Focus";
+
+        public override string ContextName => "Hotkey to Focus";
+
+        KeyboardHook keyboardHook = new KeyboardHook();
+        MouseHook mouseHook = new MouseHook();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        public Plugin()
+        {
+            //this runs once when the plugin is initialized (which is every time a project is opened)
+            keyboardHook.KeyDown += KeyboardHook_KeyDown;
+            keyboardHook.Install();
+            mouseHook.Install();
+
+            Register(KeyboardHook.VKeys.OEM_3, HotkeyCommand);
+        }
+
+        ~Plugin() {
+            keyboardHook.Uninstall();
+            mouseHook.Uninstall();
+
+            commands.Clear();
+        }
+
+
+        private Dictionary<KeyboardHook.VKeys, Action> commands = new Dictionary<KeyboardHook.VKeys, Action>();
+        internal void Register(KeyboardHook.VKeys key, Action command)
+        {
+            commands.Add(key, command);
+        }
+
+
+        public override string Initialize()
+        {
+            return base.Initialize();
+        }
+
+        private void KeyboardHook_KeyDown(KeyboardHook.VKeys key)
+        {
+            if (commands.ContainsKey(key))
+            {
+                string windowName = GetActiveWindowTitle();
+                if (windowName != null)
+                    if (windowName.ToLower().IndexOf("articy:draft") != -1)
+                    {
+                        ApplicationManifest.GetUiDispatcher().BeginInvoke((Action)(() =>
+                        {
+                            commands[key]();
+                        }));
+                    }
+            }
+        }
+
+        private void SelectAfter(AsyncTask task)
+        {
+
+        }
+
+        public override List<MacroCommandDescriptor> GetMenuEntries(List<ObjectProxy> aSelectedObjects, ContextMenuContext aContext)
+        {
+            List<MacroCommandDescriptor> result = new List<MacroCommandDescriptor>();
+            switch (aContext)
+            {
+                case ContextMenuContext.Global:
+                    // entries for the "global" commands of the ribbon menu are requested
+                    return result;
+                case ContextMenuContext.ContentArea:
+                    MacroCommandDescriptor cmd = new MacroCommandDescriptor
+                    {
+                        CaptionLid = "This is a new context menu entry!",
+                        Execute = Command,
+                    };
+                    result.Add(cmd);
+                    return result;
+                case ContextMenuContext.Other:
+                    return result;
+                case ContextMenuContext.Navigator:
+                    return result;
+                case ContextMenuContext.Search:
+                    return result;
+                case ContextMenuContext.Conflicts:
+                    return result;
+                default:
+                    // normal context menu when working in the content area, navigator, search
+                    return result;
+            }
+        }
+
+        private void Command(MacroCommandDescriptor aDescriptor, List<ObjectProxy> aSelectedObjects)
+        {
+            Debug.Write("got here");
+            //PopupNav navWindow = new PopupNav();
+            ////Session.ShowDialog(navWindow);
+            //ObjectProxy o = Session.GetProjectRoot();
+            //Session.BringObjectIntoView(o);
+
+            //AsyncTask task = new AsyncTask(null,)
+            //                PopupNav navWindow = new PopupNav();
+            //                MouseHook.POINT point = MouseHook.GetCursorPosition();
+            //                Debug.WriteLine(point.X);
+            //                Debug.WriteLine(point.Y);
+            //                Debug.WriteLine(MouseHook.GetScalingFactor());
+            //                navWindow.Top = point.Y;
+            //                navWindow.Left = point.X;
+
+        }
+        private void HotkeyCommand()
+        {
+            ObjectProxy o = null;
+            Session.SelectObject(ref o, aFilter: FilterSearchObject);
+            Session.BringObjectIntoView(o);
+        }
+
+        private bool FilterSearchObject(ObjectProxy aObject)
+        {
+            //return all objects as available to be searched
+            return true;
+        }
+
+        public override Brush GetIcon(string aIconName)
+        {
+            switch (aIconName)
+            {
+                // if you have specified the "IconFile" in the PluginManifest.xml you don't need this case
+                // unless you want to have an icon that differs when the plugin is loaded from the non-loaded case
+                // or you want to put all icons within the resources of your plugin assembly
+                /*
+                case "$self":
+                    // get the main icon for the plugin
+                    return Session.CreateBrushFromFile(Manifest.ManifestPath+"Resources\\Icon.png");
+                */
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
